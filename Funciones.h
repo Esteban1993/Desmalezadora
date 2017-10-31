da05a907c5e3f6ede004e29cbb89f3a738129b3b
Esteban <esteban.bongiovanni@outlook.com>
2017-10-31 20:00:20 -0300

    Pasaje a Puntero Completo
    Falta hacer una prueba para ver q

--- Funciones.h
+++ Funciones.h
@@ -8,12 +8,9 @@
 #ifndef FUNCIONES_H_
 #define FUNCIONES_H_
 
-
-
-#endif /* FUNCIONES_H_ */
-
-#include "Struct.h"
-#include "Defines.h"
+#include "Struct2.h"
+#include "Defines2.h"
+#include "PE_Types.h"
 /*
  * ######################## FUNCIONES ###############################
  *  * ######################## FUNCIONES ###############################
@@ -29,12 +26,14 @@ void RPM_Cero(MOTOR *motor_x);
 void Control_LC(MOTOR *motor_x);
 void CtrlPID_SetK(MOTOR motor_x);
 void CtrlPID_Control(MOTOR *motor_x);
-bool Vel_Cero(MOTOR motor_1, MOTOR motor_2, MOTOR motor_3, MOTOR motor_4);
+unsigned char Vel_Cero(MOTOR motor_1, MOTOR motor_2, MOTOR motor_3, MOTOR motor_4);
 void Tension2Duty(MOTOR *motor_x);
-void SetDuty(MOTOR motor_x);
 void Reset_PIDs(MOTOR motor_x);
-void RX(void);
-void DECODIFICADO(byte* codigo, byte inicio);
-void TX(void);
-word GrayToBin(word N);
+unsigned short GrayToBin(unsigned short N);
 void ResetVar (void);
+void RX (SERIE seriex, MOTOR *motordi, MOTOR *motordd, MOTOR *motortd, MOTOR *motorti, uint8 *ESTADO);
+
+
+
+#endif /* FUNCIONES_H_ */
+
